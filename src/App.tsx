cb7af2a43cebebe8d23163c98700486b10f95537
Felipe <felipermpires@gmail.com>
2025-10-05 14:10:11 -0300

    Tela de login

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,14 @@
-import './App.css'
+
+import "@mantine/core/styles.css"
+import { Login } from "./pages/Login"
 
 function App() {
-  
-
-  
+  return (
+    <div className="App">
+      <App />
+      <Login />
+    </div>
+  )
 }
 
 export default App
--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,29 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
+import { StrictMode } from "react"
+import ReactDOM from "react-dom/client"
+import App from "./App.tsx"
+import { MantineProvider } from "@mantine/core"
 
-createRoot(document.getElementById('root')!).render(
+//rotas
+import { createBrowserRouter, RouterProvider } from "react-router-dom"
+import { Login } from "./pages/Login.tsx"
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+  },
+  {
+    path: "/login",
+    element: <Login />,
+  },
+])
+
+// fim de rotas
+
+ReactDOM.createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    
-  </StrictMode>,
+    <MantineProvider>
+      <RouterProvider router={router} />
+    </MantineProvider>
+  </StrictMode>
 )
