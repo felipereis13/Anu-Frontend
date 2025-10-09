00ba56b17bc6c8051b5e09fbcfd2721040d881b9
Felipe <felipermpires@gmail.com>
2025-10-09 15:06:38 -0300

    Atualização tela login

--- src/pages/Login/Login.tsx
+++ src/pages/Login/Login.tsx
@@ -1,47 +1,57 @@
 import {
-  Anchor,
+  /*   Anchor,*/
   Button,
   Checkbox,
   Paper,
   PasswordInput,
-  Text,
+  /*   Text,*/
   TextInput,
   Title,
+  Image as MantineImage,
 } from "@mantine/core"
 import classes from "./Login.module.css"
+import   logoAnu  from "../../assets/anuLogo.jpeg"
 
 export function Login() {
   return (
     <div className={classes.wrapper}>
       <Paper className={classes.form}>
+        <div style={{ textAlign: "center", marginBottom: 20 }}>
+          <MantineImage
+            className={classes.logo}
+            src={logoAnu}
+            alt="Logo ANU"
+            width={2}
+            style={{ margin: "0 auto" }}
+          />
+        </div>
+
         <Title order={2} className={classes.title}>
-          Seja bem vindo ao ANU!
+          Que bom ter você de volta!
         </Title>
 
-        <TextInput
-          label="Email"
-          placeholder="Anu@gmail.com"
+        <TextInput placeholder="Digite seu e-mail" size="md" radius="md" />
+        <PasswordInput placeholder="Senha" mt="md" size="md" radius="md" />
+        <Checkbox
+          color="rgba(255, 255, 255, 1)"
+          label="Matenha-me conectado"
+          styles={{
+            label: { color: "#ffffffff" }, // Usando um código hexadecimal
+          }}
+          mt="xl"
           size="md"
-          radius="md"
+          variant="outline"
         />
-        <PasswordInput
-          label="Senha"
-          placeholder="Sua senha"
-          mt="md"
-          size="md"
-          radius="md"
-        />
-        <Checkbox label="Me mantenha conectado" mt="xl" size="md" />
-        <Button id="buttonLogin" fullWidth mt="xl" size="md" radius="md">
-          Login
+        <Button color="#a39787" fullWidth mt="xl" size="md" radius="md">
+          Entrar
         </Button>
 
-        <Text ta="center" mt="md">
+        {/*         <Text ta="center" mt="md">
           Don&apos;t have an account?{" "}
           <Anchor href="#" fw={500} onClick={(event) => event.preventDefault()}>
             Register
           </Anchor>
-        </Text>
+        </Text> */}
       </Paper>
     </div>
   )
