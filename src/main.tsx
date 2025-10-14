c0391d73bda072f2399f786533e62419f0db7e7c
Felipe <felipermpires@gmail.com>
2025-10-14 15:39:54 -0300

    Ajuste login

--- src/main.tsx
+++ src/main.tsx
@@ -6,17 +6,13 @@ import { MantineProvider } from "@mantine/core"
 //rotas
 import { createBrowserRouter, RouterProvider } from "react-router-dom"
 import { Login } from "./pages/Login/Login.tsx"
-import { EmployeeRegistrationForm } from "./pages/forms/employeeRegistrationForm.tsx"
 
 const router = createBrowserRouter([
   {
     path: "/app",
     element: <App />,
   },
-  {
-    path: "/forms",
-    element: <EmployeeRegistrationForm />,
-  },
+
   {
     path: "/",
     element: <Login />,
--- src/pages/Login/Login.tsx
+++ src/pages/Login/Login.tsx
@@ -10,49 +10,59 @@ import {
   Image as MantineImage,
 } from "@mantine/core"
 import classes from "./Login.module.css"
-import   logoAnu  from "../../assets/anuLogo.jpeg"
+import logoAnu from "../../assets/anuLogo.jpeg"
 
 export function Login() {
   return (
     <div className={classes.wrapper}>
-      <Paper className={classes.form}>
-        <div style={{ textAlign: "center", marginBottom: 20 }}>
-          <MantineImage
-            className={classes.logo}
-            src={logoAnu}
-            alt="Logo ANU"
-            width={2}
-            style={{ margin: "0 auto" }}
-          />
-        </div>
+      
+      <div>
+        <Paper className={classes.form}>
+          <div style={{ textAlign: "center", marginBottom: 20 }}>
+            <div className={classes.formContainer}>
+              <MantineImage
+                className={classes.logo}
+                src={logoAnu}
+                alt="Logo ANU"
+              />
+            </div>
 
-        <Title order={2} className={classes.title}>
-          Que bom ter você de volta!
-        </Title>
+            <Title order={2} className={classes.title}>
+              Que bom ter você de volta!
+            </Title>
 
-        <TextInput placeholder="Digite seu e-mail" size="md" radius="md" />
-        <PasswordInput placeholder="Senha" mt="md" size="md" radius="md" />
-        <Checkbox
-          color="rgba(255, 255, 255, 1)"
-          label="Matenha-me conectado"
-          styles={{
-            label: { color: "#ffffffff" }, // Usando um código hexadecimal
-          }}
-          mt="xl"
-          size="md"
-          variant="outline"
-        />
-        <Button color="#a39787" fullWidth mt="xl" size="md" radius="md">
-          Entrar
-        </Button>
+            <TextInput placeholder="Digite seu e-mail" size="md" radius="md" />
+            <PasswordInput placeholder="Senha" mt="md" size="md" radius="md" />
+            <Checkbox
+              color="rgba(255, 255, 255, 1)"
+              label="Matenha-me conectado"
+              styles={{
+                label: { color: "#ffffffff" }, // Usando um código hexadecimal
+              }}
+              mt="xl"
+              size="md"
+              variant="outline"
+            />
+            <Button color="#a39787" fullWidth mt="xl" size="md" radius="md">
+              Entrar
+            </Button>
 
-        {/*         <Text ta="center" mt="md">
+            {/*         <Text ta="center" mt="md">
           Don&apos;t have an account?{" "}
           <Anchor href="#" fw={500} onClick={(event) => event.preventDefault()}>
-            Register
+          Register
           </Anchor>
-        </Text> */}
-      </Paper>
+          </Text> */}
+          </div>
+        </Paper>
+      </div>
+      <div>
+        <MantineImage
+          className={classes.imageLogin}
+          src="/src/assets/anuBranco-capa-login.jpg"
+          alt="Login image"
+        />
+      </div>
     </div>
   )
 }
